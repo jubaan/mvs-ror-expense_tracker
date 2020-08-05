75722b498f37fe58768657650ae788fca7ccf940
jubaan <julio.ab@outlook.com>
2020-08-05 04:42:31 +0000

    Add style to views and permited parameters for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'registration', sign_out: 'logout' }
+  # devise_for :users
 
   resources :users
   resources :expenses
