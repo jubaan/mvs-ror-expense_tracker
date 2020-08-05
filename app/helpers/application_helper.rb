000abbe0b0e950c4cf37a62f7ac4b39a2cee05bf
jubaan <julio.ab@outlook.com>
2020-08-05 16:28:18 +0000

    Add partials for navbar and bootstrap js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def full_title(page_title = "")
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
+  def bootstrap_js
+    render 'layouts/bootstrap-js'
+  end
+
+  def show_navbar_if_user_login
+    render 'layouts/navbar' if user_signed_in?
+  end
 end
