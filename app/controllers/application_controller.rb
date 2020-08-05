0c587f7f185f08da94eb0dc0477b8ba9d20acaf3
jubaan <julio.ab@outlook.com>
2020-08-05 16:27:33 +0000

    Change users_path to user_path(current_user) to show user profile after sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || users_path
+    stored_location_for(resource) || user_path(current_user)
   end
 end
