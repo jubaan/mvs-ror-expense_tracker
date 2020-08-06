a1ee3550440b329067e09ac1fb1a9fe7a7e4a699
jubaan <julio.ab@outlook.com>
2020-08-06 17:45:40 +0000

    Improve layout

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,5 @@
 module UsersHelper
+  def timeline_expenses
+    Expenses.all.ordered_by_most_recent.where(user: current_user)
+  end
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,6 @@
 class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :group
+
+  scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 end
