8a8717afd405b0a01a6d64045b900089b2d2c022
jubaan <julio.ab@outlook.com>
2020-08-10 21:05:14 +0000

    Add image for users without profile pic

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,14 @@
 module UsersHelper
   def timeline_expenses
-    Expenses.includes(:groups).ordered_expenses_by_most_recent.where(user: current_user)
+    Expenses.includes(:groups).ordered_expenses_by_most_recent.where(
+      user: current_user
+    )
   end
 
   def timeline_groups
-    Groups.includes(:expenses).ordered_groups_by_most_recent.where(user: current_user)
+    Groups.includes(:expenses).ordered_groups_by_most_recent.where(
+      user: current_user
+    )
   end
 
   def user_name
@@ -20,5 +24,17 @@ module UsersHelper
 
   def total_group_current_user(group)
     current_user.expenses.where(group: group).map(&:amount).sum
+  end
+
+  def show_user_profile_image
+    if current_user.image.url.nil?
+      render html:
+               "<img src='https://lh3.googleusercontent.com/proxy/MaQ13mtUV2sn51l6Q2KFSKpWx3D2-XlQ57V4m11F3AnldwodNbucAZs74mzxzqEOAooj19CgqN3KE7RQj7v4vnPBy2TDiCqkYZ0ccRIJWuhk0w4weVvnNHtG05phd3jte6WpVhKfLo14foFt6t8A' class='rounded-circle mr-3 border' width= '150px' height= '150px'>"
+                 .html_safe
+    else
+      render =
+        image_tag current_user.image.url,
+                  width: '150px', height: '150px', class: 'rounded-circle mr-3'
+    end
   end
 end
