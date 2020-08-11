5e3d4981a2be55651bd4b51ba49d3237a84a4da4
jubaan <julio.ab@outlook.com>
2020-08-11 06:54:37 +0000

    Create scopes for grouped and not_grouped expenses

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,7 +2,8 @@ class Expense < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :group
 
-  scope :ordered_expenses_by_most_recent, -> { order(created_at: :desc) }
+  scope :grouped_ordered_expenses_by_most_recent, -> { where('group_id <> 1').order(created_at: :desc) }
+  scope :not_grouped_ordered_expenses_by_most_recent, -> { where('group_id = 1').order(created_at: :desc) }
 
   validates :name, :amount, presence: true
   validates :group, presence: true, allow_nil: true, allow_blank: true
