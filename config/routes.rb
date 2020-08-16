7832693f7800b634a8f292bb37ddf013372d5779
jubaan <julio.ab@outlook.com>
2020-08-16 17:10:35 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # devise_for :users
 
   resources :users, only: %i[show edit update delete]
-  resources :expenses, except: %i[show]
+  resources :expenses
   resources :groups
 
   root 'pages#welcome'
