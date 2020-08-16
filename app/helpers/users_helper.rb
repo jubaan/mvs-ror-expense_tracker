3ce8529c50ad77b5863d8a7a3878eb738dd617f4
jubaan <julio.ab@outlook.com>
2020-08-16 20:01:23 +0000

    Add image for when a user doestn have uploades a photo

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,7 +9,7 @@ module UsersHelper
   def show_user_profile_image
     if current_user.image.url.nil?
       render html:
-               "<img src='https://lh3.googleusercontent.com/proxy/MaQ13mtUV2sn51l6Q2KFSKpWx3D2-XlQ57V4m11F3AnldwodNbucAZs74mzxzqEOAooj19CgqN3KE7RQj7v4vnPBy2TDiCqkYZ0ccRIJWuhk0w4weVvnNHtG05phd3jte6WpVhKfLo14foFt6t8A' class='rounded-circle mr-3 border' width= '150px' height= '150px'>"
+               "<img src='https://i2.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1' class='rounded-circle mr-3 border' width= '150px' height= '150px'>"
                  .html_safe
     else
       render = image_tag current_user.image.url, width: '150px', height: '150px', class: 'rounded-circle mr-3'
